62b1912708c1405f1df0745c0cfd51fdc00720f6
EyreFree <zheng.xu03@ele.me>
2017-04-20 20:31:11 +0800

    Setup macOS Example

---
+++ Examples/macOS/macOS Example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOS Example
+//
+//  Created by EyreFree on 2017/4/20.
+//  Copyright © 2017年 EyreFree. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
