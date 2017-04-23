a2c7696c9bdf59a143f1fa40b86ee34f306fcbdf
EyreFree <eyrefree@eyrefree.org>
2017-04-23 16:04:05 +0800

    Setup tvOS Example

---
+++ Examples/tvOS/tvOS Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tvOS Example
+//
+//  Created by EyreFree on 2017/4/23.
+//  Copyright © 2017年 EyreFree. All rights reserved.
+//
+
+import UIKit
+import EFQRCode
+
+class ViewController: UIViewController {
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
