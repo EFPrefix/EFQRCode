e14f39474a7bb408c560e103e7ae975b2dac8b94
EyreFree <eyrefree@eyrefree.org>
2019-09-11 12:51:29 +0800

    Fix #65

---
+++ Startup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+if ! command -v carthage > /dev/null; then
+	printf 'Carthage is not installed.\n'
+	printf 'See https://github.com/Carthage/Carthage for install instructions.\n'
+	exit 1
+fi
+carthage update --platform iOS,macOS,tvOS,watchOS --no-use-binaries
+# carthage build --platform iOS,macOS,tvOS,watchOS --no-skip-current
+
+if ! command -v pod > /dev/null; then
+	printf 'CocoaPods is not installed.\n'
+	printf 'See https://github.com/CocoaPods/CocoaPods for install instructions.\n'
+	exit 1
+fi
+cd Examples;
+pod repo update;
+pod install;
