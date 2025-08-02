4b0fde5e093ee5ba74523bf45faedc50fbda4f7f
EyreFree <eyrefree@eyrefree.org>
2025-08-02 22:59:03 +0800

    rm: codecov

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -52,7 +52,7 @@ class Tests: XCTestCase {
         // This is an example of EFQRCodeGenerator test case.
         let content = "https://github.com/EFPrefix/EFQRCode"
         let generator = try? EFQRCode.Generator(content, style: .basic(params: .init()))
-        let testResult = try? generator?.toImage(width: 256).cgImage
+        let testResult = try? generator?.toImage(width: 256).cgImage()
         XCTAssertNotNil(testResult, "testResult is nil!")
 
         // This is an example of EFQRCodeRecognizer test case.
@@ -65,7 +65,7 @@ class Tests: XCTestCase {
         // This is an example of EFQRCodeGenerator test case.
         let content = ""
         let generator = try? EFQRCode.Generator(content, style: .bubble(params: .init()))
-        let testResult = try? generator?.toImage(width: 180).cgImage
+        let testResult = try? generator?.toImage(width: 180).cgImage()
         XCTAssertNotNil(testResult, "testResult is nil!")
 
         // This is an example of EFQRCodeRecognizer test case.
@@ -77,8 +77,8 @@ class Tests: XCTestCase {
     func testExample3() {
         // This is an example of EFQRCodeGenerator test case.
         let content = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624406566430860213949463952247371907021798609437027705392171762931767523846748184676694051320005681271452635608277857713427577896091736371787214684409012249534301465495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989"
-        let generator = try? EFQRCode.Generator(content, style: .bubble(params: .init()))
-        let testResult = try? generator?.toImage(width: 180).cgImage
+        let generator = try? EFQRCode.Generator(content, style: .dsj(params: .init()))
+        let testResult = try? generator?.toImage(width: 800).cgImage()
         XCTAssertNotNil(testResult, "testResult is nil!")
 
         // This is an example of EFQRCodeRecognizer test case.
@@ -96,7 +96,7 @@ class Tests: XCTestCase {
                 image: .init(image: .static(image: getImage()), allowTransparent: true))
             )
         )
-        let testResult = try? generator?.toImage(width: 180).cgImage
+        let testResult = try? generator?.toImage(width: 180).cgImage()
         XCTAssertNotNil(testResult, "testResult is nil!")
     }
 
