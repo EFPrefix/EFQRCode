545a424dee5f8b79fcddea8225c6c61b467c375d
EyreFree <eyrefree@eyrefree.org>
2025-08-02 17:38:49 +0800

    ci: xcodebuild

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 //
 //  Package.swift
 //  EFQRCode
@@ -33,7 +33,8 @@ let package = Package(
         .macOS(.v10_15),
         .iOS(.v13),
         .tvOS(.v13),
-        .watchOS(.v6)
+        .watchOS(.v6),
+        .visionOS(.v1)
     ],
     products: [
         .library(name: "EFQRCode", targets: ["EFQRCode"])
